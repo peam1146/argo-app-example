89634a6296a3eccd42db694788b8f116738a513c
Peam <peammy2010@hotmail.com>
2022-06-27 18:29:15 +0700

    update version

--- app/main.go
+++ app/main.go
@@ -10,7 +10,7 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
